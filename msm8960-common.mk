b2b03d397e1f7587894fef2ec07187c94b5b9187
rik0612c <qq872638399@gmail.com>
2023-08-12 22:23:49 +0800

    msm8960-common: Build Aperture

--- msm8960-common.mk
+++ msm8960-common.mk
@@ -98,7 +98,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl \
     camera.msm8960 \
-    libmmcamera_interface2
+    libmmcamera_interface2 \
+    Aperture
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
